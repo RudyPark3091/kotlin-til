57583dbc6330500500353ccd62820fb202efcb1d
RudyPark3091 <grayblack313@gmail.com>
2020-12-22 02:10:12 +0900

    Update regex/Json.kt

--- regex/Json.kt
+++ regex/Json.kt
@@ -11,9 +11,23 @@ fun main(args: Array<String>) {
 	var matchResult = regex.find(jsonData)
 
 	// 비구조화 할당 또는 구조 분해 할당 이라 함
-	// MatchResult 객체의 destructed는 정규 표현식 그룹을 반환
-	val (key, value) = matchResult!!.destructed
+	// MatchResult 객체의 destructured는 정규 표현식 그룹을 반환
+	val (key, value) = matchResult!!.destructured
 
-	println("key" + key)
-	println("value" + value)
+	println("key " + key)
+	println("value " + value) 
+	println()
+
+	jsonData = """
+{
+	"name": "HM",
+	"alias": "RudyPark3091",
+	"lang": "kotlin"
+}
+	"""
+
+	// replace문을 사용하면 json형식 string을 한줄로 압축할 수 있음
+	println("raw json string " + jsonData)
+	var shortJson = jsonData.replace("\\s*".toRegex(), "")
+	println("shortened json string " + shortJson)
 }
